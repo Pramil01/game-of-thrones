a396c003e32fcb47e7d2db1bab935c7de771196a
Pramil Kesarwani <Kesarwanipramil01@gmail.com>
2021-05-27 02:40:39 +0530

    Title image corrected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import SearchBar from './components/SearchBar';
 import ImageList from './components/ImageList';
+import Got from './img/Got.jpg';
 
 let style={
   width:'100%',
   height:'10% !important',
-  padding:' 0px 30px'
+  padding:' 0px 30px',
+  color:'#ffffff'
 }
 class App extends React.Component {
   state = {isLoaded: false,item: [] ,fullItems:[]};
@@ -13,7 +15,6 @@ class App extends React.Component {
         const response = await fetch('https://thronesapi.com/api/v2/Characters');
         const data = await response.json();
         this.setState({isLoaded:true,item:data,fullItems:data});
-        console.log(data);
       }
   onSearchChange =(term)=>{
     if(term.length!==0){
@@ -27,7 +28,7 @@ class App extends React.Component {
     const{isLoaded , item } = this.state;
     return (
       <div  style={{padding : '20px 0px',backgroundColor: '#000000',minHeight:'100vh'}}>
-        <img alt="GotLogo" src="/Got.jpg" style={style}/>
+        <img alt="GotLogo" src={Got} style={style}/>
         <SearchBar onChange={this.onSearchChange}/>
         <br />
         <ImageList isLoaded={isLoaded} item={item}/>
